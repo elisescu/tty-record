faf4f7d8078651654870f56bee2019586eb0074e
Vasile Popescu <elisescu@elisescu.com>
2017-11-18 20:14:42 +0100

    Fix the DOM element that will host xterm.js

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,5 @@
 import Player from './player.js'
-const player = new Player(document.getElementById('#terminal'), scriptData);
+const player = new Player(document.getElementById('terminal'), scriptData);
 player.play();
 
 // document.getElementById("test_button").onclick = function() {
